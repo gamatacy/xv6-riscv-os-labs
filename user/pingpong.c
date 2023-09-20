6da846598b946824509bf7d4082b1827ea25af15
gamatacy <voroninivan003@gmail.com>
2023-09-20 13:20:04 +0300

    fix pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -20,19 +20,31 @@ main(){
     if(child_pid == 0){
         char buf[sizeof PING];
 
+        close(p[1]);
+        close(c[0]);
+
         read(p[0], buf, sizeof buf);
         fprintf(1, "%d: got %s\n", self_pid, buf);
         write(c[1], PONG, sizeof PONG);
 
+        close(p[0]);
+        close(c[1]);
+
+
     }else{
         char buf[sizeof PONG];
+
+        close(p[0]);
+        close(c[1]);
 
         write(p[1], PING, sizeof PING);
         wait( (int * ) 0);
         read(c[0], buf, sizeof buf);
         fprintf(1, "%d: got %s\n", self_pid, buf);
 
-        for (int i = 0; i < 2; ++i) {close(p[i]); close(c[i]);}
+        close(p[0]);
+        close(c[1]);
+
     }
 
     exit(0);
